9655a5933bb59ac643f36cf38dd256f7570de3c2
Yasaman <mansouriyasaman82@gmail.com>
2021-08-07 00:11:41 +0430

    feat network list

--- components/Sidebar/AdminSidebar.vue
+++ components/Sidebar/AdminSidebar.vue
@@ -175,6 +175,13 @@ export default {
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>`
         },
+        {
+          name: "Networks",
+          link: "/admin/network",
+          icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.111 16.404a5.5 5.5 0 017.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.141 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0" />
+</svg>`
+        },
       ],
     };
   },
---
+++ pages/admin/network/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div
+    class="h-screen p-8 mx-auto bg-background"
+  >
+    <h2
+      class="mb-10 text-3xl font-bold uppercase text-H1"
+    >
+      Networks
+    </h2>
+<!--    <create-modal-->
+<!--      v-if="showCreateModal"-->
+<!--      :selected-storage="selectedStorage"-->
+<!--      :clusters="clusters"-->
+<!--      :is-edit="isEdit"-->
+<!--    ></create-modal>-->
+    <storage-list
+      :is-loading="isLoading"
+      :networks="networks"
+      :is-end="isEnd"
+      :key="networks.length"
+    ></storage-list>
+  </div>
+</template>
+
+<script>
+  import storageList from '/components/network/list'
+  import createModal from '/components/network/create-modal'
+  export default {
+    data() {
+      return {
+        clusters: [],
+        networks: [],
+        isLoading: false,
+        showCreateModal: false,
+        showModal: false,
+        selectedStorage: {},
+        isEdit: false,
+        pageIndex: 1,
+        isEnd: false
+      }
+    },
+    components: {
+      storageList,
+      createModal
+    },
+    created () {
+      this.loadData()
+      this.getClusters()
+
+      this.$nuxt.$on('closeModal', () => {
+        this.selectedStorage = {}
+        this.isEdit = false
+        this.toggleModal()
+      })
+      this.$nuxt.$on('toggleCreateModal', () => {
+        this.isEdit = false
+        this.toggleCreateModal()
+      })
+      this.$nuxt.$on('onLoadData', () => {
+        this.pageIndex = 0
+        this.networks = []
+        this.loadData()
+      })
+      this.$nuxt.$on('onSetStorage', (data) => {
+        this.isEdit = true
+        this.selectedStorage = data
+      })
+      this.$nuxt.$on('loadMore', () => {
+        this.pageIndex++
+        this.loadData()
+      })
+    },
+    methods: {
+      loadData () {
+        this.isLoading = true
+        this.$axios.$get('backend/network/index?page=' + this.pageIndex)
+          .then(response => {
+            this.networks = this.networks.concat(response.data);
+            if (response.links.next === null) {
+              this.isEnd = true
+            } else {
+              this.isEnd = false
+            }
+            this.isLoading = false
+          })
+      },
+      getClusters () {
+        this.$axios.$get('backend/cluster/index')
+          .then(response => {
+            this.clusters = response.data
+          })
+      },
+      toggleModal () {
+        this.showModal = !this.showModal
+      },
+      toggleCreateModal () {
+        this.showCreateModal = !this.showCreateModal
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
