9e756f408b7a24146dbf3c790c34e180585730e5
Yasaman <mansouriyasaman82@gmail.com>
2021-08-08 16:34:55 +0430

    feat create pool

--- components/Sidebar/AdminSidebar.vue
+++ components/Sidebar/AdminSidebar.vue
@@ -196,6 +196,13 @@ export default {
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
 </svg>`
         },
+        {
+          name: "Pools",
+          link: "/admin/pool",
+          icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
+</svg>`
+        },
       ],
     };
   },
---
+++ pages/admin/pool/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div
+    class="h-screen p-8 mx-auto bg-background"
+  >
+    <h2
+      class="mb-10 text-3xl font-bold uppercase text-H1"
+    >
+      Pools
+    </h2>
+<!--    <create-modal-->
+<!--      v-if="showCreateModal"-->
+<!--      :selected-network="selectedNetwork"-->
+<!--      :clusters="clusters"-->
+<!--      :is-edit="isEdit"-->
+<!--    ></create-modal>-->
+    <pool-list
+      :is-loading="isLoading"
+      :pools="pools"
+      :is-end="isEnd"
+      :key="pools.length"
+    ></pool-list>
+  </div>
+</template>
+
+<script>
+  import poolList from '/components/pool/list'
+  import createModal from '/components/network/create-modal'
+  export default {
+    data() {
+      return {
+        clusters: [],
+        pools: [],
+        isLoading: false,
+        showCreateModal: false,
+        showModal: false,
+        selectedNetwork: {},
+        isEdit: false,
+        pageIndex: 1,
+        isEnd: false
+      }
+    },
+    components: {
+      poolList,
+      createModal
+    },
+    created () {
+      this.loadData()
+      this.getClusters()
+
+      this.$nuxt.$on('closeModal', () => {
+        this.selectedNetwork = {}
+        this.isEdit = false
+        this.toggleModal()
+      })
+      this.$nuxt.$on('toggleCreateModal', () => {
+        this.isEdit = false
+        this.toggleCreateModal()
+      })
+      this.$nuxt.$on('onLoadData', () => {
+        this.pageIndex = 0
+        this.pools = []
+        this.loadData()
+      })
+      this.$nuxt.$on('onSetNetwork', (data) => {
+        this.isEdit = true
+        this.selectedNetwork = data
+      })
+      this.$nuxt.$on('loadMore', () => {
+        this.pageIndex++
+        this.loadData()
+      })
+    },
+    methods: {
+      loadData () {
+        this.isLoading = true
+        this.$axios.$get('backend/pool/index?page=' + this.pageIndex)
+          .then(response => {
+            this.pools = this.pools.concat(response.data);
+            if (response.links.next === null) {
+              this.isEnd = true
+            } else {
+              this.isEnd = false
+            }
+            this.isLoading = false
+          })
+      },
+      getClusters () {
+        this.$axios.$get('backend/cluster/index')
+          .then(response => {
+            this.clusters = response.data
+          })
+      },
+      toggleModal () {
+        this.showModal = !this.showModal
+      },
+      toggleCreateModal () {
+        this.showCreateModal = !this.showCreateModal
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
