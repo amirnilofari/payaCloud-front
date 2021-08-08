a10c0ee4a78c77030c1b46c960282b638d79de52
Arni <39923791+amirnilofari@users.noreply.github.com>
2021-08-08 23:18:53 +0430

    node-list

---
+++ components/node/list.vue
@@ -0,0 +1,176 @@
+
+<template>
+  <div>
+    <div class="px-4 py-6 mb-0 bg-white border-0 rounded-t">
+      <div class="flex flex-wrap items-center">
+        <div class="flex flex-wrap items-center w-full max-w-full px-4">
+          <h3
+            class="inline text-lg font-semibold text-primary"
+          >
+            Nodes List
+          </h3>
+          <button
+            @click="close"
+            type="button"
+            class="absolute inline-flex justify-center w-full px-10 py-2 mt-3 mr-6 text-base font-medium rounded-md shadow-sm right-16 bg-primary text-background focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            create node
+          </button>
+        </div>
+      </div>
+    </div>
+    <loading
+      v-if="isLoading"
+      class="mt-14"
+    ></loading>
+    <empty-state
+      v-else-if="!isLoading && nodes.length === 0"
+    ></empty-state>
+    <table
+      v-else
+      class="items-center w-full bg-white border-collapse rounded shadow-lg"
+    >
+      <thead
+        class="bg-background"
+      >
+      <tr>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Id
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Network Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Package Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Pool Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Section Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+          Storage Name
+        </th>
+        <th
+          class="px-6 py-3 text-sm font-semibold text-left uppercase align-middle border border-l-0 border-r-0 border-primary text-primary whitespace-nowrap"
+        >
+
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="node in nodes"
+        :key="node.id"
+        class="cursor-pointer hover:bg-background"
+      >
+        <td
+          class="p-4 px-6 text-xs font-bold align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.id}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.network.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.package.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.pool.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.section.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          {{node.storage.name}}
+        </td>
+        <td
+          class="p-4 px-6 text-xs align-middle border-t-0 border-l-0 border-r-0 text-H3 whitespace-nowrap"
+        >
+          <button
+            class="p-2 mr-5 text-white rounded-full bg-primary hover:shadow-lg"
+            @click="onEdit(node)"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            </svg>
+          </button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <div
+      class="mt-6 text-center"
+    >
+      <button
+        v-if="!isEnd"
+        @click="getData"
+        class="px-6 py-3 mb-1 mr-1 text-sm font-semibold transition-all duration-150 ease-linear rounded shadow outline-none bg-background text-primary hover:shadow-lg focus:outline-none"
+        type="submit"
+      >
+        Load More
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import loading from '/components/loading'
+  import emptyState from '/components/empty-state'
+
+  export default {
+    components: {
+      loading,
+      emptyState
+    },
+    props: ['isLoading', 'nodes', 'isEnd'],
+    methods: {
+      onEdit (node) {
+        this.$nuxt.$emit('toggleCreateModal')
+        this.$nuxt.$emit('onSetNode', node)
+      },
+      close () {
+        this.$nuxt.$emit('toggleCreateModal')
+      },
+      getData () {
+        this.$nuxt.$emit('loadMore')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/admin/node/index.vue
@@ -0,0 +1,146 @@
+<template>
+  <div
+    class="h-screen p-8 mx-auto bg-background"
+  >
+    <h2
+      class="mb-10 text-3xl font-bold uppercase text-H1"
+    >
+      Nodes
+    </h2>
+    <create-modal
+      v-if="showCreateModal"
+      :selected-node="selectedNode"
+      :networks="networks"
+      :packags="packags"
+      :pools="pools"
+      :sections="sections"
+      :storages="storages"
+      :is-edit="isEdit"
+    ></create-modal>
+    <node-list
+      :is-loading="isLoading"
+      :nodes="nodes"
+      :is-end="isEnd"
+      :key="nodes.length"
+    ></node-list>
+  </div>
+</template>
+
+<script>
+  import nodeList from '/components/node/list'
+  import createModal from '/components/node/create-modal'
+  export default {
+    data() {
+      return {
+        nodes: [],
+        networks: [],
+        packags: [],
+        pools: [],
+        sections: [],
+        storages: [],
+        isLoading: false,
+        showCreateModal: false,
+        showModal: false,
+        selectedNode: {},
+        isEdit: false,
+        pageIndex: 1,
+        isEnd: false
+      }
+    },
+    components: {
+      nodeList,
+      createModal
+    },
+    created () {
+      this.loadData()
+      this.getNetworks()
+      this.getPackags()
+      this.getPools()
+      this.getSections()
+      this.getStorages()
+
+      this.$nuxt.$on('closeModal', () => {
+        this.selectedNode = {}
+        this.isEdit = false
+        this.toggleModal()
+      })
+      this.$nuxt.$on('toggleCreateModal', () => {
+        this.isEdit = false
+        this.toggleCreateModal()
+      })
+      this.$nuxt.$on('onLoadData', () => {
+        this.pageIndex = 0
+        this.networks = []
+        this.packags = []
+        this.pools = []
+        this.sections = []
+        this.storages = []
+        this.loadData()
+      })
+      this.$nuxt.$on('onSetNode', (data) => {
+        this.isEdit = true
+        this.selectedNode = data
+      })
+      this.$nuxt.$on('loadMore', () => {
+        this.pageIndex++
+        this.loadData()
+      })
+    },
+    methods: {
+      loadData () {
+        this.isLoading = true
+        this.$axios.$get('backend/node/index?page=' + this.pageIndex)
+          .then(response => {
+            this.nodes = this.nodes.concat(response.data);
+            if (response.links.next === null) {
+              this.isEnd = true
+            } else {
+              this.isEnd = false
+            }
+            this.isLoading = false
+          })
+      },
+      getNetworks () {
+        this.$axios.$get('backend/network/index')
+          .then(response => {
+            this.networks = response.data
+          })
+      },
+      getPackags () {
+        this.$axios.$get('backend/package/index')
+          .then(response => {
+            this.packags = response.data
+          })
+      },
+      getPools () {
+        this.$axios.$get('backend/pool/index')
+          .then(response => {
+            this.pools = response.data
+          })
+      },
+      getSections () {
+        this.$axios.$get('backend/section/index')
+          .then(response => {
+            this.sections = response.data
+          })
+      },
+      getStorages () {
+        this.$axios.$get('backend/storage/index')
+          .then(response => {
+            this.storages = response.data
+          })
+      },
+      toggleModal () {
+        this.showModal = !this.showModal
+      },
+      toggleCreateModal () {
+        this.showCreateModal = !this.showCreateModal
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
