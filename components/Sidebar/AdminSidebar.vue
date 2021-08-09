8950897a44c401d9887eabaa1d31eaba03a4581b
Arni <39923791+amirnilofari@users.noreply.github.com>
2021-08-09 17:55:40 +0430

    create-ip-section

--- components/Sidebar/AdminSidebar.vue
+++ components/Sidebar/AdminSidebar.vue
@@ -232,6 +232,13 @@ export default {
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
 </svg>`
         },
+        {  
+          name: "IPs",
+          link: "/admin/address",
+          icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+</svg>`
+        },
         
       ],
     };
---
+++ components/address/create-modal.vue
@@ -0,0 +1,225 @@
+<template>
+  <transition name="slide">
+    <div
+      class="fixed inset-0 z-10 overflow-y-auto details-modal" role="dialog" aria-modal="true">
+      <div class="flex items-end justify-center w-full min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div class="inline-block text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:mt-4 sm:align-middle sm:max-w-6xl sm:w-full">
+          <form
+            class="p-6"
+          >
+            <h3 class="mb-6 text-xl font-semibold text-H3">New IP</h3>
+            <div class="flex flex-wrap">
+              <div class="w-full px-4 mb-2 lg:w-6/12">
+                <div class="relative w-full mb-3">
+                  <label
+                    class="block mb-2 text-xs font-bold uppercase text-primary"
+                  >
+                    Address
+                  </label>
+                  <input
+                    v-model="address"
+                    type="text"
+                    class="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border rounded shadow border-secondary placeholder-secondary text-H1 focus:outline-none focus:ring"
+                    placeholder="Address"
+                  />
+                </div>
+              </div>
+              <div class="w-full px-4 mb-2 lg:w-6/12">
+                <div class="relative w-full mb-3">
+                  <label
+                    class="block mb-2 text-xs font-bold uppercase text-primary"
+                  >
+                    Gateway
+                  </label>
+                  <input
+                    v-model="gateway"
+                    type="text"
+                    class="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border rounded shadow border-secondary placeholder-secondary text-H1 focus:outline-none focus:ring"
+                    placeholder="Gateway"
+                  />
+                </div>
+              </div>
+              
+            </div>
+            <div class="flex flex-wrap">
+                <div class="w-full px-4 mb-2 lg:w-6/12">
+                <div class="relative w-full mb-3">
+                  <label
+                    class="block mb-2 text-xs font-bold uppercase text-primary"
+                  >
+                    Netmask
+                  </label>
+                  <input
+                    v-model="netmask"
+                    type="text"
+                    class="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border rounded shadow border-secondary placeholder-secondary text-H1 focus:outline-none focus:ring"
+                    placeholder="Netmask"
+                  />
+                </div>
+              </div>
+
+              <div class="w-full px-4 mb-2 lg:w-6/12">
+                <div class="relative w-full mb-3">
+                  <label
+                    class="block mb-2 text-xs font-bold uppercase text-primary"
+                  >
+                    Package
+                  </label>
+                  <select
+                    v-model="packageId"
+                    class="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border rounded shadow border-secondary placeholder-secondary text-secondary focus:outline-none focus:ring">
+                    <option value="" disabled selected>Package</option>
+                    <option
+                      v-for="packag in packages"
+                      :key="packag.id"
+                      :value="packag.id"
+                    >
+                      {{packag.name}}
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+            </div>
+            <div class="flex flex-wrap">
+              <div class="w-full px-4 mb-2">
+                <div class="relative w-full mt-7">
+                  <button
+                    @click="close"
+                    type="button"
+                    class="px-16 py-3 mb-1 mr-1 mr-3 text-sm font-bold uppercase transition-all duration-150 ease-linear border rounded shadow outline-none bg-background text-primary border-primary hover:shadow-lg focus:outline-none"
+                  >
+                    cancel
+                  </button>
+                  <button
+                    @click="submitForm"
+                    type="button"
+                    class="px-16 py-3 mb-1 mr-1 text-sm font-bold uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-primary text-background hover:shadow-lg focus:outline-none"
+                  >
+                    submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: '',
+        address: '',
+        gateway: '',
+        netmask: '',
+        packageId: ''
+      }
+    },
+    props: ['isEdit', 'selectedAddress', 'packages'],
+    methods: {
+      close() {
+        this.$nuxt.$emit('toggleCreateModal')
+      },
+      submitForm () {
+        let formdata = new FormData()
+        formdata.append('address', this.address)
+        formdata.append('gateway', this.gateway)
+        formdata.append('netmask', this.netmask)
+        formdata.append('packageId', this.packageId)
+
+        if (this.isEdit) {
+          this.$axios.$post('backend/address/update/' + this.id,
+            formdata)
+            .then(response => {
+              if (response.message) {
+                this.$toast.error(response.message)
+              } else {
+                this.$nuxt.$emit('onLoadData')
+                this.$toast.success('Successfully edited!')
+              }
+              this.close()
+            })
+        } else {
+          this.$axios.$post('backend/address/create',
+            formdata)
+            .then(response => {
+              if (response.message) {
+                this.$toast.error(response.message)
+              } else {
+                this.$nuxt.$emit('onLoadData')
+                this.$toast.success('Successfully created!')
+              }
+              this.close()
+            })
+        }
+      }
+    },
+    created() {
+      if (this.isEdit) {
+        this.id = this.selectedAddress.id
+        this.address = this.selectedAddress.address
+        this.gateway = this.selectedAddress.gateway
+        this.netmask = this.selectedAddress.netmask
+        this.packageId = this.selectedAddress.package.id
+      } else {
+        this.id = ''
+        this.address = ''
+        this.gateway = ''
+        this.netmask = ''
+        this.packageId = ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .details-modal .image-cont{
+    top: -60px;
+    position: absolute;
+  }
+  .slide-enter {
+    opacity: 1;
+  }
+
+  .slide-enter-active {
+    opacity: 1;
+    animation: slide-in 0.5s ease-out forwards;
+    transition: opacity 0.5s;
+  }
+
+  .slide-leave-to {
+    opacity: 1;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 0.5s ease-out forwards;
+    opacity: 0;
+    transition: opacity 0.5s;
+  }
+
+  @keyframes slide-in {
+    0% {
+      transform: translateY(-15px);
+    }
+    30% {
+      transform: translateY(15px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(-30px);
+    }
+  }
+</style>
