55e07348e77b1fb19eb0af3a7503ab137d7de161
Arni <39923791+amirnilofari@users.noreply.github.com>
2021-08-15 21:57:39 +0430

    upadate-navbar

--- components/Navbar/AdminNavbar.vue
+++ components/Navbar/AdminNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Navbar -->
   <nav
-    class="absolute top-0 left-0 z-10 flex items-center w-full p-4 bg-transparent md:flex-row md:flex-nowrap md:justify-start"
+    class="absolute top-0 left-0 z-10 flex items-center w-full p-4 bg-transparent md:flex-row md:bg-primary md:flex-nowrap md:justify-start"
   >
     <div
       class="flex flex-wrap items-center justify-between w-full px-4 mx-autp md:flex-nowrap md:px-10"
